16cf5dd1e6de8c027ccbeb24ccaa17426939b0ac
FOBshippingpoint <aulaulql4ql4xj6@gmail.com>
2022-04-04 05:04:00 +0800

    fix scripts

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,14 +1,14 @@
 import ghpages from "gh-pages";
 import fs from "fs";
 
-const indexHtml = fs.readFileSync("../dist/index.html", "utf8");
+const indexHtml = fs.readFileSync("./dist/index.html", "utf8");
 
 const regex = /="\//g;
 if (regex.test(indexHtml)) {
   console.log("Found relative path in index.html, fixing...");
   const newHtml = indexHtml.replace(/="\//g, '="./');
   console.log("Writing new index.html...");
-  fs.writeFileSync("../dist/index.html", newHtml);
+  fs.writeFileSync("./dist/index.html", newHtml);
   console.log("Done!");
 }
 
